9087ca6ce001110d52b2eec98470f32a0b37c0ce
ccymbor <ccymbor@gmail.com>
2013-06-19 17:52:30 -0700

    init

---
+++ StringPermutations/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+            var result = FindPermutations(line);
+            Console.WriteLine(result);
+        }
+    }
+
+    public static string FindPermutations(string line)
+    {
+        var ret = new List<string>();
+
+        ret = PermuteString(line.ToCharArray(), 0);
+
+        ret.Sort();
+
+        return ret.Aggregate((s1, s2) => (s1 + "," + s2));
+    }
+
+    private static List<string> PermuteString(char [] line, int location)
+    {
+        var ret = new List<string>();
+        if (line.Length == location)
+        {
+            ret.Add(new String(line));
+            return ret;
+        }
+
+        char currentChar = line[location];
+
+        for (int i = location; i < line.Length; i++)
+        {
+            //Swap char
+            line[location] = line[i];
+            line[i] = currentChar;
+
+            ret.AddRange(PermuteString(line, location+1));
+
+            //restore the char for the next iteration
+            line[i] = line[location];
+            line[location] = currentChar;
+        }
+
+        return ret;
+    }
+}
---
+++ StringPermutationsTests/StringPermutationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringPermutationsTests
+{
+    [TestClass]
+    public class StringPermutationTest
+    {
+        [TestMethod]
+        public void Testhat()
+        {
+            var ret = Program.FindPermutations("hat");
+            Assert.AreEqual("aht,ath,hat,hta,tah,tha", ret);
+        }
+    }
+}
